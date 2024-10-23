37fb651e13d92439aa9f796c105a8b53bc2542b8
ramya <ramya.pekatanoju@gmail.com>
2024-10-23 10:24:18 +0530

    adding files

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "ami" {
+   type        = string
+   default     = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
